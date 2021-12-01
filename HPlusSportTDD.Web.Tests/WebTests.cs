adf4fbeea0719a5d791f37268d6c0fe693cfaab9
Robert Hardy <helloarchy@gmail.com>
2021-12-01 20:42:59 +0000

    Add test project for web frontend

---
+++ HPlusSportTDD.Web.Tests/WebTests.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+using HPlusSportTDD.Core;
+using HPlusSportTDD.Web.Controllers;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace HPlusSportTDD.Web.Tests
+{
+    public class WebTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+    }
+}
