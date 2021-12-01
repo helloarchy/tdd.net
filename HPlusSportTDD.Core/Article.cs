cb4afea0b164ae3c1f159198fafed3ccb1d4b397
Robert Hardy <helloarchy@gmail.com>
2021-12-01 20:38:22 +0000

    Add web frontend project

---
+++ HPlusSportTDD.Core/Article.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel;
+
+namespace HPlusSportTDD.Core
+{
+    public class Article
+    {
+        [DisplayName("SKU")]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+        
+        public Article()
+        {
+        }
+    }
+}
---
+++ HPlusSportTDD.Web/Controllers/ShopController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using HPlusSportTDD.Core;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HPlusSportTDD.Web.Controllers
+{
+    public class ShopController : Controller
+    {
+        private List<Article> _articles = new() {
+            new Article { Id=1, Name="Red T-Shirt", Price=9.99},
+            new Article { Id=2, Name="Blue T-Shirt", Price=11.99},
+            new Article { Id=3, Name="Green Windbreaker", Price=99.99}
+        };
+
+        public IActionResult Index(string query)
+        {
+            var model = _articles;
+            if (!string.IsNullOrWhiteSpace(query))
+            {
+                model = _articles.Where(a => a.Name.ToLower().Contains(query.ToLower())).ToList();
+            }
+
+            return View(model);
+        }
+
+    }
+}
