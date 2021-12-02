2872a63b6de1e9361829ba4c8eee6d96e5b4b7f4
Robert Hardy <helloarchy@gmail.com>
2021-12-02 11:52:28 +0000

    Add API project

---
+++ HPlusSportTDD.API/Controllers/ArticlesController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using HPlusSportTDD.Core;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HPlusSportTDD.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ArticlesController : ControllerBase
+    {
+        private static List<Article> _articles = new() {
+            new Article { Id=1, Name="Red T-Shirt", Price=9.99},
+            new Article { Id=2, Name="Blue T-Shirt", Price=11.99},
+            new Article { Id=3, Name="Green Windbreaker", Price=99.99}
+        };
+
+        [HttpGet]
+        public IEnumerable<Article> GetAll()
+        {
+            return _articles;
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetSingle(int id)
+        {
+            var article = _articles.Find(a => a.Id == id);
+            
+            return Ok(article);
+        }
+    }
+}
